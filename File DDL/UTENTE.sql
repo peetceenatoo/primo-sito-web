3373f8086f3ab7427270f68723983cab83080d9c
Mattia <piccinatomattia@gmail.com>
2023-05-25 10:58:48 +0200

    change UTENTE.sql

--- File DDL/UTENTE.sql
+++ File DDL/UTENTE.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `primo-sito-web`.`UTENTE` (
-`Nome` varchar(45) NOT NULL,
-`Cognome` varchar(45) NOT NULL,
 `Email` varchar(45) NOT NULL,
 `Password` varchar(45) NOT NULL,
+`Nome` varchar(45) NOT NULL,
+`Cognome` varchar(45) NOT NULL,
 `Indirizzo` varchar(200) NOT NULL,
 PRIMARY KEY (`Email`)
 )
