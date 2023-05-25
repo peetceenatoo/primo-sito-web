e018252aa31e6d57c507dc7bf2803415267bad23
Mattia <piccinatomattia@gmail.com>
2023-05-25 11:01:24 +0200

    Create initializers

---
+++ primo-sito-web-PURE/src/main/java/it/polimi/tiw/Utilities/TemplateInitializer.java
@@ -0,0 +1,22 @@
+package it.polimi.tiw.Utilities;                                                            
+                                                                                                          
+import org.thymeleaf.TemplateEngine;                                                                      
+import org.thymeleaf.templatemode.TemplateMode;                                                           
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;                                     
+import org.thymeleaf.web.servlet.JavaxServletWebApplication;                                              
+                                                                                                          
+public abstract class TemplateInitializer {                                                                   
+                                                                                                          
+    public static TemplateEngine getTemplateEngine(JavaxServletWebApplication application){               
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+        templateResolver.setTemplateMode(TemplateMode.HTML);                                              
+        templateResolver.setPrefix("WEB-INF/");                                                           
+        templateResolver.setSuffix(".html");                                                              
+        templateResolver.setCharacterEncoding("UTF-8");                                                   
+        TemplateEngine templateEngine = new TemplateEngine();                                             
+        templateEngine.setTemplateResolver(templateResolver);                                             
+                                                                                                          
+        return templateEngine;                                                                            
+    }                                                                                                     
+}                                                                                                         
+                                                                                                          
