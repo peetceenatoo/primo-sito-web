6ed86074da30731238ff5c5613ec9a3dc430a46e
Mattia <piccinatomattia@gmail.com>
2023-05-31 15:41:21 +0200

    Fix FasciaDiSpedizione

--- primo-sito-web-PURE/src/main/java/it/polimi/tiw/Bean/FasciaDiSpedizione.java
+++ primo-sito-web-PURE/src/main/java/it/polimi/tiw/Bean/FasciaDiSpedizione.java
@@ -1,5 +1,5 @@
 package it.polimi.tiw.Bean;
 
-public record FasciaDiSpedizione(int id, int idFornitore, int min, int max, double prezzo ) {
+public record FasciaDiSpedizione(int id, int idFornitore, int min, Integer max, double prezzo ) {
 	
 }
