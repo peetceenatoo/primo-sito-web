94867ddd1e0bb3d43f13c9f1618a074ccc239a8b
Mattia <piccinatomattia@gmail.com>
2023-06-01 17:04:32 +0200

    Add filters

---
+++ primo-sito-web-RIA/src/main/java/it/polimi/tiw/Filter/FiltroDefault_SeNonLoggato.java
@@ -0,0 +1,52 @@
+package it.polimi.tiw.Filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/")
+public class FiltroDefault_SeNonLoggato implements Filter {
+
+    private ServletContext context;
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        this.context = config.getServletContext();
+    }
+    
+    @Override
+    public void doFilter(ServletRequest richiesta, ServletResponse risposta, FilterChain chain) throws IOException, ServletException {
+    	// faccio il cast della richiesta prendo la sessione
+    	HttpServletRequest richiestaHTTP = (HttpServletRequest) richiesta;
+        HttpSession sessione = richiestaHTTP.getSession();
+
+        // se l'utente non è loggato
+        if( sessione.isNew() || ( sessione.getAttribute("utente") == null ) ){
+        	// e se ha chiesto la risorsa "/"
+        	if( ( richiestaHTTP.getPathInfo() == null ) || ( richiestaHTTP.getPathInfo() == "" ) || ( richiestaHTTP.getPathInfo() == "/" ) ){
+        		// mando il redirect affinchè il percorso "/" rimandi al file di default login.html
+        		((HttpServletResponse) risposta).sendRedirect(richiestaHTTP.getContextPath() + "/login.html");
+        		return;
+        	}
+        }
+        // altrimenti 404
+        if( ( richiestaHTTP.getPathInfo() != "/css/stylesheet.css" ) && ( richiestaHTTP.getPathInfo() != "/js/utils.js" ) ){
+        	((HttpServletResponse) risposta).setStatus(HttpServletResponse.SC_FORBIDDEN);
+        	return;
+        }
+        
+        // mando al file
+        chain.doFilter(richiesta, risposta);
+    }
+
+}
---
+++ primo-sito-web-RIA/src/main/java/it/polimi/tiw/Filter/Filtro_SoloSeLoggato.java
@@ -0,0 +1,36 @@
+package it.polimi.tiw.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
+@WebFilter({"/ultimiVisualizzati", "/carrello", "/ordini", "/listaPrezzi", "/risultati", "/visualizza", "/immagine", "/home-page.html", "/js/home-page.js"})
+public class Filtro_SoloSeLoggato implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest richiesta, ServletResponse risposta, FilterChain chain) throws IOException, ServletException {
+    	// faccio il cast della richiesta prendo la sessione
+    	HttpServletRequest richiestaHTTP = (HttpServletRequest) richiesta;
+        HttpSession sessione = richiestaHTTP.getSession();
+        
+        // se la sessione per l'utente non è attiva, ritorno un errore
+        if( sessione.isNew() || ( sessione.getAttribute("utente") == null ) ){
+            ((HttpServletResponse)risposta).setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+
+        // mando alla servlet/file
+        chain.doFilter(richiesta, risposta);
+    }
+
+}
